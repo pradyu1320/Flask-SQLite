ddf058cd1c0069a759246e9da8d78667c559a424
Pradyumna Vinjamuri <vinjumup@meredith.com>
2021-02-19 07:07:13 +0530

    Initial commit

---
+++ init_db.py
@@ -0,0 +1,13 @@
+import sqlite3
+
+connection = sqlite3.connect('database.db')
+
+with open('schema.sql') as f:
+    connection.executescript(f.read())
+
+cur = connection.cursor()
+
+cur.execute("INSERT INTO posts (title,content) VALUES(?,?)",('First post','Content for first post'))
+cur.execute("INSERT INTO posts (title,content) VALUES(?,?)",('Second post','Content for second post'))
+connection.commit()
+connection.close()
